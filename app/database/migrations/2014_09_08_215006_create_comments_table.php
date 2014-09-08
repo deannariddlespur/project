d0f8b66af6a87926d7bb1013e6542f40bcf62694
webdevdea <webdevdea@gmail.com>
2014-09-08 18:56:41 -0400

    Create comments table migration

--- app/database/migrations/2014_09_08_215006_create_comments_table.php
+++ app/database/migrations/2014_09_08_215006_create_comments_table.php
@@ -12,7 +12,16 @@ class CreateCommentsTable extends Migration {
 	 */
 	public function up()
 	{
-		//
+		Schema::create('comments', function(Blueprint $table) {
+		    $table->increments('id');
+		    $table->unsignedInteger('post_id');
+		    $table->string('commenter');
+		    $table->string('email');
+		    $table->text('comment');
+		    $table->boolean('approved');
+		    $table->timestamps();
+	    });
+		    
 	}
 
 	/**
@@ -22,7 +31,7 @@ class CreateCommentsTable extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('comments');
 	}
 
 }
