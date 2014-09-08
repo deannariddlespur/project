fafe1139978f4566aecfe73df6e44388b2a65a56
webdevdea <webdevdea@gmail.com>
2014-09-08 19:02:07 -0400

    Create Models

---
+++ app/models/Comment.php
@@ -0,0 +1,9 @@
+<?php
+// file: app/models/Comment.php
+ class Comment extends Eloquent {
+  
+       public function post()
+       {
+           return $this->belongsTo('Post');
+        }
+}
---
+++ app/models/Post.php
@@ -0,0 +1,9 @@
+<?php
+// file: app/models/Post.php
+ class Post extends Eloquent {
+  
+       public function comments()
+       {
+           return $this->hasMany('Comment');
+        }
+}
